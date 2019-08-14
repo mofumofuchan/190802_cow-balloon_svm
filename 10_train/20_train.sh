1b7eee78bc330ad435edd79d3894c9479d216b70
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2019-08-14 20:19:26 +0900

    Add files

---
+++ 10_train/20_train.sh
@@ -0,0 +1,11 @@
+TIMESTAMPS="
+172137139645586-2019-03-15-18-00
+172137139645586-2019-05-11-17-00
+172137139645586-2019-03-30-13-00
+66999552851844-2019-05-13-11-00"
+
+
+for time_stamp in $TIMESTAMPS; do
+python train_svm.py --config  00_configs/calving.ini \
+                    --time_stamp $time_stamp
+done
--- 10_train/train_svm.py
+++ 10_train/train_svm.py
@@ -7,26 +7,40 @@ from tqdm import tqdm
 
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True)
+    parser.add_argument("--time_stamp", required=True)
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
     config.read(args.config)
 
-    neg_npy_fnames = os.listdir(config["train"]["neg_path"])
-    neg_npy_list = [np.load(os.path.join(config["train"]["neg_path"], fname))
+    print(args.time_stamp)
+
+    neg_npy_fnames = os.listdir(os.path.join(config["train"]["neg_rootpath"],
+                                             args.time_stamp,
+                                             "no"))
+    neg_npy_list = [np.load(os.path.join(config["train"]["neg_rootpath"],
+                                         args.time_stamp,
+                                         "no",
+                                         fname))
                     for fname in neg_npy_fnames]
 
-    pos_npy_fnames = os.listdir(config["train"]["pos_path"])
-    pos_npy_list = [np.load(os.path.join(config["train"]["pos_path"], fname))
+    pos_npy_fnames = os.listdir(os.path.join(config["train"]["neg_rootpath"],
+                                             args.time_stamp,
+                                             "yes"))
+    pos_npy_list = [np.load(os.path.join(config["train"]["pos_rootpath"],
+                                         args.time_stamp,
+                                         "yes",
+                                         fname))
                     for fname in pos_npy_fnames]
-    
+
     X = []
     y = []
 
@@ -36,11 +50,14 @@ def main():
     X.extend(pos_npy_list)
     y.extend([1 for _ in pos_npy_list])
 
-    # TODO 正規化
+    #  正規化
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X_normed = scaler.transform(X)
 
     random_state = int(config["DEFAULT"]["random_state"])
     X_train, X_test, \
-        y_train, y_test = train_test_split(X, y,
+        y_train, y_test = train_test_split(X_normed, y,
                                            test_size=0.1,
                                            random_state=random_state)
     tuned_parameters = [
@@ -54,30 +71,58 @@ def main():
          'gamma': [0.001, 0.0001]}
     ]
     score = "f1"
-    clf = GridSearchCV(
+    gscv = GridSearchCV(
         SVC(),
         tuned_parameters,
         cv=5,
-        scoring=f"{score}_weighted"
+        scoring=f"{score}_weighted",
+        verbose=1
     )
 
-    clf.fit(X_train, y_train)
+    gscv.fit(X_train, y_train)
+    clf = gscv.best_estimator_
 
     # AUC計算
 
-
-    neg_npy_fnames = os.listdir(config["eval"]["neg_path"])
-    neg_npy_list = [np.load(os.path.join(config["eval"]["neg_path"], fname))
+    neg_npy_fnames = os.listdir(os.path.join(config["train"]["neg_rootpath"],
+                                             args.time_stamp,
+                                             "no"))
+    neg_npy_list = [np.load(os.path.join(config["train"]["neg_rootpath"],
+                                         args.time_stamp,
+                                         "no",
+                                         fname))
                     for fname in neg_npy_fnames]
 
-    pos_npy_fnames = os.listdir(config["eval"]["pos_path"])
-    pos_npy_list = [np.load(os.path.join(config["eval"]["pos_path"], fname))
+    pos_npy_fnames = os.listdir(os.path.join(config["train"]["neg_rootpath"],
+                                             args.time_stamp,
+                                             "yes"))
+    pos_npy_list = [np.load(os.path.join(config["train"]["neg_rootpath"],
+                                         args.time_stamp,
+                                         "yes",
+                                         fname))
                     for fname in pos_npy_fnames]
 
+    X = []
+    y = []
 
-    # 正規化
+    X.extend(neg_npy_list)
+    y.extend([0 for _ in neg_npy_list])
+
+    X.extend(pos_npy_list)
+    y.extend([1 for _ in pos_npy_list])
+
+    #  正規化
+    scaler.fit(X)
+    X_normed = scaler.transform(X)
 
     # AUC計算
+    y_pred = clf.predict(X_normed)
+
+    auc = roc_auc_score(y, y_pred)
+
+    with open(f"{args.config}_{args.time_stamp}_score.txt",
+              mode="w") as f:
+        f.write(f"[{args.time_stamp}] auc: {auc}")
 
 
 if __name__ == "__main__":
